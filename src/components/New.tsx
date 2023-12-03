a873d0247adafe3bd218683ab6043eed56313eee
joaovcostas <jvc22102003@gmail.com>
2023-12-03 09:35:27 -0300

    hidden url

--- src/components/New.tsx
+++ src/components/New.tsx
@@ -31,7 +31,7 @@ export default function New() {
   const R = 'http://192.168.58.118:80'
   const J = 'http://192.168.76.118:80'
 
-  const baseUrl = J
+  const baseUrl = ''
 
   const { calibInfo, setCalibInfo } = useWeightContext()
 
